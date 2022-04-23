c64c25f141ccdc3d7595aa0db823a0c819bc6d38
mykyta.bondarchuk <mykyta.bondarchuk@playwing.com>
2022-04-23 21:55:28 +0300

    Add some Twiggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route(path: "/", name: "homepage")]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        return $this->render('vinyl/homepage.html.twig');
     }
 
     #[Route(path: "/browse/{slug}", name: "browse")]
